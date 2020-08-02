10207a09b259c121f0ecf7289b08d74adfe28f52
Alexander <aleksandrcw@gmail.com>
2020-08-02 15:06:15 +0300

    Add extension to work with animated images in UIImage.Add test image on main screen.

--- GifSearch/ViewController.swift
+++ GifSearch/ViewController.swift
@@ -12,10 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let a = NetworkService()
-        a.request(byText: "cats") { (data, error) in
+        let networkService = NetworkService()
+        networkService.request(byText: "cats") { (data, error) in
             
         }
+        let image = UIImage.gifImageWithURL("https://media0.giphy.com/media/cfuL5gqFDreXxkWQ4o/giphy.gif?cid=f50b613d8m0rlawg42e544y7vz2ycprt54bz7id3z1c7lp4y&rid=giphy.gif")
+        let imageView = UIImageView(image: image)
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
 
 
